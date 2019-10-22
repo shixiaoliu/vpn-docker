1842a781df2727b757930b54940069f776a5fe1b
shiliu <liu.shi@dtise.com>
2019-10-22 14:09:28 +0800

    commit openvpn client

---
+++ openvpn-client/README.md
@@ -0,0 +1,8 @@
+#simple openvpn client
+
+## example in code
+```
+remote ip 192.168.1.0/24
+authfile  passwd
+ovpnfile  server.ovpn
+```
---
+++ openvpn-client/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -o errexit
+
+dir="/root/openvpn"
+auth="$dir/passwd"
+conf="$dir/server.ovpn"
+mkdir -p /dev/net
+[[ -c /dev/net/tun ]] || mknod -m 0666 /dev/net/tun c 10 200
+[[ -e $conf ]] || { echo "ERROR: VPN not configured!"; sleep 120; }
+[[ -e $cert ]] || { echo "ERROR: VPN CA cert missing!"; sleep 120; }
+iptables -t nat -A POSTROUTING -o tun0 -d 192.168.1.0/24 -j MASQUERADE
+exec sg vpn -c "openvpn --cd $dir --config $conf --auth-user-pass $auth"
