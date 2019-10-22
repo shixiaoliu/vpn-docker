2bd9835b164c0b350467fcb56a030884a9254123
shiliu <liu.shi@dtise.com>
2019-10-22 15:02:18 +0800

    tun+ 统配

--- openvpn-client/entrypoint.sh
+++ openvpn-client/entrypoint.sh
@@ -8,5 +8,5 @@ mkdir -p /dev/net
 [[ -c /dev/net/tun ]] || mknod -m 0666 /dev/net/tun c 10 200
 [[ -e $conf ]] || { echo "ERROR: VPN not configured!"; sleep 120; }
 [[ -e $cert ]] || { echo "ERROR: VPN CA cert missing!"; sleep 120; }
-iptables -t nat -A POSTROUTING -o tun0 -d 192.168.1.0/24 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o tun+ -d 192.168.1.0/24 -j MASQUERADE
 exec sg vpn -c "openvpn --cd $dir --config $conf --auth-user-pass $auth"
