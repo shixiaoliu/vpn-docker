73fc2b76b15c1eaf50b88de4acb1b85cc3e3b983
shiliu <liu.shi@dtise.com>
2019-12-16 11:52:40 +0800

    适应新版本openvpn server

--- openvpn-client/entrypoint.sh
+++ openvpn-client/entrypoint.sh
@@ -7,6 +7,6 @@ conf="$dir/server.ovpn"
 mkdir -p /dev/net
 [[ -c /dev/net/tun ]] || mknod -m 0666 /dev/net/tun c 10 200
 [[ -e $conf ]] || { echo "ERROR: VPN not configured!"; sleep 120; }
-[[ -e $cert ]] || { echo "ERROR: VPN CA cert missing!"; sleep 120; }
-iptables -t nat -A POSTROUTING -o tun+ -d 192.168.100.0/24 -j MASQUERADE
-exec sg vpn -c "openvpn --cd $dir --config $conf --auth-user-pass $auth"
+[[ -e $auth ]] || { echo "ERROR: auth file missing!"; sleep 120; }
+iptables -t nat -A POSTROUTING -o tun+ -d 192.168.1.0/24 -j MASQUERADE
+exec sg vpn -c "openvpn --cd $dir --config $conf --askpass $auth --auth-nocache"
